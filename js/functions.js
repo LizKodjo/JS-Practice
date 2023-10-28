20fbe4baf3b3a646eb7c53941d7bbad12a8b620c
Elizabeth Kodjo <ellizakodjo@outlook.com>
2023-10-28 21:01:27 +0100

    Created object with an array

--- js/functions.js
+++ js/functions.js
@@ -8,6 +8,12 @@ const recipeName = document.getElementById("cocktailName");
 const ingredient1 = document.getElementById("mainAlcohol");
 const ingredient2 = document.getElementById("secondaryAlcohol");
 const ingredient3 = document.getElementById("mixers");
+const form = document.getElementById("form");
+
+// prevent default
+form.addEventListener('click', (e) => {
+    e.preventDefault();
+});
 
 //Add a click event listener to the button
 addButton.addEventListener("click", addRecipe);
@@ -19,18 +25,35 @@ let recipes = [];
 /////////////////////////////////////////////////////
 
 function addRecipe() {
+   
     ////////////////////////////////////////////////////////
     //Make a "recipe" object here.
     //The "recipe" object, has a name, and an  ingredients array only.
     //The name, should come from the cocktailName input.
     //The ingredients array, will be whatever is in the ingredient 1, 2, and 3 inputs.
     //Then push this recipe to the recipes array.
-    ////////////////////////////////////////////////////////
+    ////////////////////////////////////////////////////////    
 
     //Write your code below
+
+    // Recipe object
+
+    const recipe = {
+        // name
+        name: recipeName.value,
+        // array
+        ingredients: [ingredient1.value, ingredient2.value, ingredient3.value]
+    };
+    console.log(recipe);
+
+
+    recipes.push(recipe);
+    console.log(recipes);
 
     console.log("Recipe name: " + recipeName.value);
     console.log("Main Alcohol: " + ingredient1.value);
     console.log("Secondary Alcohol: " + ingredient2.value);
     console.log("Mixers: " + ingredient3.value);
+
+    form.reset();
 }
